72cceae75c332264780e3362d2b2c7506784e67c
Kovacsik László Ádám <kovacsiklaszloadam@gmail.com>
2025-01-13 22:27:40 +0100

    First function

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,7 +1,12 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = (currentResult + 10);
+function addNumbers(num1, num2) {
+    const result = num1 + num2;
+    return result;
+}
+
+currentResult = addNumbers(1, 2);;
 
 let calculationDescription = `${defaultResult} + 10`
 outputResult(currentResult, calculationDescription);
