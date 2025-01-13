007e97868622c3808696b921fafb305d04fcd64c
Kovacsik László Ádám <kovacsiklaszloadam@gmail.com>
2025-01-13 18:41:40 +0100

    First calculation

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,1 +1,7 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult = (currentResult + 10);
+
+let calculationDescription = "(0 = 10)"
+outputResult(currentResult, calculationDescription);
