8100ac12371d4d5ddb38060c4a498e3dfeade9c7
Kovacsik László Ádám <kovacsiklaszloadam@gmail.com>
2025-01-14 23:58:48 +0100

    Indirectly calling a function

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,12 +1,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function addNumbers(num1, num2) {
-    const result = num1 + num2;
-    return result;
+function addNumbers() {
+    currentResult = currentResult + userInput.value;
+    outputResult(currentResult, '');
 }
 
-currentResult = addNumbers(1, 2);;
-
-let calculationDescription = `${defaultResult} + 10`
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener('click', addNumbers);
