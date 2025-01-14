d3054ef0b123c50f6e028f75d82a4e4a865ba137
Kovacsik László Ádám <kovacsiklaszloadam@gmail.com>
2025-01-15 00:35:44 +0100

    Adding two numbers together completed

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,9 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getEnteredNumber() {
+    return parseInt(userInput.value);
+}
+
 function addNumbers() {
-    currentResult = currentResult + userInput.value;
-    outputResult(currentResult, '');
+    const enteredNumber = getEnteredNumber();
+    const description = `${currentResult} + ${enteredNumber}`;
+    currentResult = currentResult + enteredNumber;
+    outputResult(currentResult, description);
 }
 
 addBtn.addEventListener('click', addNumbers);
