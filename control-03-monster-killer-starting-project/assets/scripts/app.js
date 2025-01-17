da3232d2689e56277e1f1bc99296e30d79927361
kovacsiklaszloadam <116884013+kovacsiklaszloadam@users.noreply.github.com>
2025-01-17 12:56:28 +0100

    Strong attack button functionality added

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
@@ -21,4 +22,19 @@ function attackHandler() {
     }
 }
 
-attackBtn.addEventListener('click', attackHandler)
+function strongAttackHandler() {
+    const monsterDamage = dealMonsterDamage(STRONG_ATTACK_VALUE);
+    currentMonsterHealth -= monsterDamage;
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You won!');
+    } else if(currentPlayerHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You lost!');
+    } else if(currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+        alert('Draw!');
+    }
+}
+
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
