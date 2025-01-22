a18abd70583df7ac1f229a8308966dd8485d1b95
Kovacsik László Ádám <kovacsiklaszloadam@gmail.com>
2025-01-22 23:45:44 +0100

    Third assigment completed

---
+++ control-assignment-problem/assignment.js
@@ -0,0 +1,22 @@
+const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
+const randomNumber2 = Math.random();
+
+if (randomNumber > 0.7) {
+    alert('The number is greater than 0.7');
+}
+
+const numbers = [1, 2, 3, 4, 5];
+
+for (let i = numbers.length; i > 0; i--) {
+    console.log(numbers[i - 1]);
+}
+
+for (el of numbers) {
+    console.log(el);
+}
+
+if (randomNumber > 0.7 && randomNumber2 > 0.7) {
+    alert('Both numbers are greater than 0.7');
+} else if (randomNumber < 0.2 || randomNumber2 < 0.2) {
+    alert('One of the numbers is smaller than 0.2');
+}
