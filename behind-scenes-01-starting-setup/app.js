3913c42c76f60a0be84fe648c816967a0f2e21f0
Kovacsik László Ádám <kovacsiklaszloadam@gmail.com>
2025-01-26 17:37:39 +0100

    Testing the difference between defining variables and constants with var, let and const

---
+++ behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,16 @@
+var name = 'Max';
+
+if(name === 'Max') {
+    let hobbies = ['sports', 'cooking'];
+    console.log(hobbies);
+}
+
+function greet() {
+    var age = 30;
+    var name = 'Manuel';
+    console.log(name, age);
+}
+
+console.log(name, hobbies);
+
+greet();
