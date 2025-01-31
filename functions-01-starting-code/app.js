5b24775975e0c9fb1ed0abdde366e6d018016b83
Kovacsik László Ádám <kovacsiklaszloadam@gmail.com>
2025-01-31 12:27:27 +0100

    Player can select their choice for the game

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -1,1 +1,24 @@
 const startGameBtn = document.getElementById('start-game-btn');
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFUAULT_USER_CHOICE = 'ROCK';
+
+const getPlayerChoice = function() {
+    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
+    if (
+        selection !== ROCK && 
+        selection !== PAPER && 
+        selection !== SCISSORS
+    ) {
+        alert(`Invalid choice! We chose ${DEFUAULT_USER_CHOICE} for you!`);
+        return DEFUAULT_USER_CHOICE;
+    }
+    return selection;
+}
+
+startGameBtn.addEventListener('click', function() {
+    console.log('Starting game...');
+    const playerSelection = getPlayerChoice();
+    console.log(playerSelection);
+});
