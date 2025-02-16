2d2dfd68ff5f7a9df8dfc5c902c3032df7af4998
Kovacsik László Ádám <kovacsiklaszloadam@gmail.com>
2025-02-16 18:07:53 +0100

    Fifth assignment solved

---
+++ dom-assignment-start/assignment.js
@@ -0,0 +1,13 @@
+const task1 = document.getElementById("task-1");
+const task1Again = document.querySelector("li");
+
+task1.style.backgroundColor = "black";
+task1Again.style.color = "white";
+
+const title = document.querySelector("title");
+const titleAgain = document.head.querySelector("title");
+
+titleAgain.textContent = "Assingment - Solved!";
+
+const h1 = document.querySelector("h1");
+h1.textContent = "Assignment - Solved!";
